caa0e47aaed65b408f22f114f46fa07e77b46f58
ARYAN JAISWAL <143169615+ARYAN-3345@users.noreply.github.com>
2026-05-01 19:55:23 +0530

    Update greeting message in Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Ant build!");
+        System.out.println("Hello from Ant build! and aryan");
     }
 }
